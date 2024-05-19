78f9c7173f07788a693554394ac2dbf9a1763307
DavisLin <davislin@star-link.tech>
2024-05-19 15:53:16 +0800

    feat(main-page): finish 九宮格動畫
    The Spec
    九宮格需要垂直置中於下方區塊
    格子樣式: height: 100px;border: black solid 2px;background: radial-gradient(circle, rgba(113,81,95,1)
    81%, rgba(0,0,0,1) 100%);
    指定格子(右上, 正中, 右下) 會不停閃爍 (需有漸變or淡入淡出效果)
    指定的球 (四個角落) ->  w&h: 30px; bg:#A5F12B;
    球往右方移動時會在所有九宮格的上方，不被任一九宮格蓋住，且全程保持透明度 100%

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+    import Content from '@/components/Content.vue'
+    import Header from '@/components/Header.vue'
+    import Sidebar from '@/components/Sidebar.vue'
+    import { ref } from 'vue'
+
+    const showMenu = ref(false)
+    const handleToggleMenu = () => {
+        showMenu.value = !showMenu.value
+    }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <Header @toggle-menu="handleToggleMenu" />
+    <Content />
+    <Sidebar v-model="showMenu" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+    html,
+    body,
+    #app {
+        width: 100%;
+        height: 100%;
+    }
+    body {
+        margin: 0;
+    }
+    #app {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+    }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,60 @@
+<script setup>
+    import { defineComponent } from 'vue'
+    defineComponent({
+        name: 'PageContent',
+    })
+</script>
+
+<template>
+    <div class="content w-full h-full px-1 flex items-center justify-center bg-gray-300 overflow-hidden">
+        <div class="w-full grid grid-cols-3 gap-1">
+            <div
+                v-for="(num, index) in 9"
+                class="relative flex items-center justify-center h-[100px]"
+                :key="`cell-${num}`"
+            >
+                <div
+                    class="cell z-10 w-full h-full border-2 border-black"
+                    :class="{
+                        breath: index % 2 === 0 && index % 3 !== 0,
+                    }"
+                ></div>
+                <div
+                    v-if="(num < 4 || num > 6) && num % 2 !== 0"
+                    class="ball w-full h-full absolute z-20 flex items-center justify-center"
+                >
+                    <div class="flex items-center justify-center w-[30px] h-[30px] bg-[#A5F12B] rounded-full">0</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .cell {
+        background: radial-gradient(circle, rgba(113, 81, 95, 1) 81%, rgba(0, 0, 0, 1) 100%);
+        &.breath {
+            animation: breath 450ms ease infinite;
+        }
+    }
+    @keyframes breath {
+        from {
+            opacity: 1;
+        }
+        to {
+            opacity: 0.6;
+        }
+    }
+
+    .ball {
+        animation: moving 1000ms ease infinite;
+    }
+    @keyframes moving {
+        from {
+            transform: translate3d(0, 0, 0);
+        }
+        to {
+            transform: translate3d(200%, 0, 0);
+        }
+    }
+</style>
