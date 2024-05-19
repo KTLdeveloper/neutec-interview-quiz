5189c8f5cdc5b4702fddfaafbe0f2f51d31e4945
DavisLin <davislin@star-link.tech>
2024-05-19 12:44:13 +0800

    feat(initial): setting eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
+import pluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import parserVue from "vue-eslint-parser";
 
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  pluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+        ...globals.node,
+      },
+      ecmaVersion: 2020,
+      parser: parserVue,
+    },
+  },
 ];
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin({
-    include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
-  })],
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
+    })
+  ],
 })
