25553bf028476d131bc282775b876ffb6d1d1f01
DavisLin <davislin@star-link.tech>
2024-05-19 15:53:50 +0800

    feat(main-page): finish 側邊選單收合按鈕

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import SidebarMenu from '@/components/Icons/SidebarMenu.vue'
+    import { defineComponent, defineEmits } from 'vue'
+    defineComponent({
+        name: 'PageHeader',
+    })
+    defineEmits(['toggle-menu'])
+</script>
+
+<template>
+    <div class="header flex items-center justify-end h-10 p-1">
+        <div
+            class="w-10 h-full flex items-center justify-center bg-gray-300 border border-black rounded"
+            @click="$emit('toggle-menu')"
+        >
+            <SidebarMenu class="w-5 h-5" />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Icons/SidebarMenu.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import { defineComponent } from 'vue'
+
+    defineComponent({
+        name: 'SidebarMenu',
+    })
+</script>
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M8 12h8M6 18h12"
+        />
+    </svg>
+</template>
